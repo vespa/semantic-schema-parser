8060bd4b31b7a788e7bd9195ee976bc9ad00b666
vespa <d.vespa@gmail.com>
2014-03-18 16:36:22 -0300

    now, we can have the analysis of multiple URLs in the same report

--- example/main.js
+++ example/main.js
@@ -4,18 +4,22 @@
 
     //Requiring files
     fs = require('fs');
-    schema = require ('./schemaParser');
+    schema = require ('../lib/schemaParser');
+
+
+    var urls  = [
+        "http://www.imdb.com/title/tt0096874/",
+        "https://www.baby.com.br/produtos/tenis-xadrez-grafite-com-velcro-converse-all-star",
+        "http://www.walmart.com.br/produto/Telefonia/iPhone/Apple/413770-Apple-iPhone-4S-8GB-Preto-Desbloqueado",
+        "http://www.foodnetwork.com/recipes/ree-drummond/cajun-chicken-pasta-recipe.html",
+        "https://www.iba.com.br/revista-digital/Computer-Arts-Brasil-Mar%C3%A7o-2014-79-f574542cc39d3cb876bacca97bb39689",
+        "http://www.ebay.com/itm/Doctor-Who-3D-TARDIS-Police-Box-Pewter-Tall-PENDANT-20-Long-Chain-Necklace-/331098975095?pt=LH_DefaultDomain_0&hash=item4d17096b77"
+        ];
+
+
 
     //Reading files
-    schema.parser([
-        "http://www.imdb.com/title/tt0096874/"
-        //"https://www.baby.com.br/produtos/tenis-xadrez-grafite-com-velcro-converse-all-star"
-      //"http://www.walmart.com.br/produto/Telefonia/iPhone/Apple/413770-Apple-iPhone-4S-8GB-Preto-Desbloqueado",
-       //"http://www.foodnetwork.com/recipes/ree-drummond/cajun-chicken-pasta-recipe.html"
-      //"https://www.iba.com.br/revista-digital/Computer-Arts-Brasil-Mar%C3%A7o-2014-79-f574542cc39d3cb876bacca97bb39689"
-        //"http://www.ebay.com/itm/Doctor-Who-3D-TARDIS-Police-Box-Pewter-Tall-PENDANT-20-Long-Chain-Necklace-/331098975095?pt=LH_DefaultDomain_0&hash=item4d17096b77"
-        ], function(msg){
-
+    schema.parseURLs(urls, function(msg){
        fs.writeFile("../result.json", msg, function(err){
           if(err){
             console.log("report: something gones wrong:" +err);
